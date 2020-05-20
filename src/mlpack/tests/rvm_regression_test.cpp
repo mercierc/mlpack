41f6075eb5d504f46024a9836184085aa56d4a2a
cmercier <clement.mercier@greentropism.com>
2020-05-20 09:00:20 +0200

    Update CMakeLists.txt.

---
+++ src/mlpack/tests/rvm_regression_test.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+// Includes all relevant components of mlpack.
+
+#include <math.h>
+#include <ctime>
+
+#include <mlpack/core/data/load.hpp>
+
+#define BOOST_TEST_DYN_LINK 
+#define BOOST_TEST_MODULE Mytest
+#include <boost/test/unit_test.hpp>
+
+
+#include "rvm_regression.hpp"
+
+using namespace mlpack;
+
+BOOST_AUTO_TEST_CASE(RVMRegressionTest)
+{
+  BOOST_REQUIRE(true)
+}
